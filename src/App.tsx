10c7bd05f8e2db24ba32167970b80dfc92a828fe
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-11 23:56:23 +0900

    追加 APIでデータの取得を実装

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { FC, useEffect, useState } from "react";
+import { ListItem } from "./components/ListItem";
+import axios from "axios";
+import type { User } from "./type/user";
+
+export const App: FC = () => {
+	const [users, setUsers] = useState<User[]>([]);
+
+	//画面表示時にユーザー情報取得
+	useEffect(() => {
+		axios.get<User[]>("https://example.com/users").then((res) => {
+			setUsers(res.data);
+		});
+	});
+
+	return (
+		<div>
+			{users.map((user) => (
+				<ListItem id={user.id} name={user.name} age={user.age} />
+			))}
+		</div>
+	);
+};
--- src/components/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-	return <h1>こんにちは！</h1>;
-}
-
-export default App;
