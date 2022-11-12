1935591fc0dc3cdb4cf9e9f0172ce0b760b044a6
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-12 21:06:00 +0900

    削除 API利用時に作成したファイルやコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { FC, useEffect, useState } from "react";
-import { ListItem } from "./components/ListItem";
-import axios from "axios";
-import type { User } from "./type/user";
+import { FC } from "react";
 
 export const App: FC = () => {
-	const [users, setUsers] = useState<User[]>([]);
-
-	//画面表示時にユーザー情報取得
-	useEffect(() => {
-		axios.get<User[]>("https://example.com/users").then((res) => {
-			setUsers(res.data);
-		});
-	});
-
 	return (
 		<div>
 			<h1>今日は</h1>
-			{users.map((user) => (
-				<ListItem id={user.id} name={user.name} age={user.age} />
-			))}
 		</div>
 	);
 };
