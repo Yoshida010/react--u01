4851578867a989065ead377160cb218c2b74ecbc
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-12 21:13:04 +0900

    追加 カウントアップの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
+import { useState } from "react";
 
 export const App: FC = () => {
+	const [count, setCount] = useState(0);
+
+	const onClickCountUp = () => {
+		setCount(count + 1);
+	};
+
 	return (
 		<div>
-			<h1>今日は</h1>
+			<h1>カウントアップ</h1>
+			<p>{count}</p>
+			<button onClick={onClickCountUp}>count</button>
 		</div>
 	);
 };
