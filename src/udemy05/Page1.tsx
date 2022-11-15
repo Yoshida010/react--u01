82b432064eea6feab3b89676fd4a3c4a5e088ab5
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-15 20:05:55 +0900

    追加 useHistoryの追加

--- src/udemy05/Page1.tsx
+++ src/udemy05/Page1.tsx
@@ -1,14 +1,21 @@
 /** @format */
 
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1: FC = () => {
+	const arr = [...Array(100).keys()];
+	console.log(arr);
+
+	const history = useHistory();
+	const onClickDetailA = () => history.push("/page1/detailA");
+
 	return (
 		<div>
 			<h1>Page1</h1>
-			<Link to="/page1/detailA">Page1DetailA</Link>
+			<Link to={{ pathname: "/page1/detailA", state: arr }}>Page1DetailA</Link>
 			<Link to="/page1/detailB">Page1DetailB</Link>
+			<button onClick={onClickDetailA}>DetailA</button>
 		</div>
 	);
 };
--- src/udemy05/Page1DetailA.tsx
+++ src/udemy05/Page1DetailA.tsx
@@ -1,11 +1,20 @@
 /** @format */
 
 import { FC } from "react";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA: FC = () => {
+	const { state } = useLocation();
+	console.log(state);
+
+	// ブラウザバックと同じことができる！
+	const history = useHistory();
+	const onClickBack = () => history.goBack();
+
 	return (
 		<div>
 			<h1>Page1DetailA</h1>
+			<button onClick={onClickBack}>戻る</button>
 		</div>
 	);
 };
